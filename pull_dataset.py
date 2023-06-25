6cad709063278aa77d088f64f1668f00a5d4926e
Basia <barbara.sikora@hotmail.com>
2023-06-25 15:16:47 +0100

    did an example df and added another env var

--- pull_dataset.py
+++ pull_dataset.py
@@ -5,14 +5,14 @@ from io import BytesIO
 
 
 
-def pull(kaggle_username, kaggle_apikey):
+def pull(kaggle_username, kaggle_apikey, kaggle_dataset_path):
     #API bit
     #with kaggle you need to authenticate using basic Auth, which is a type of auth.
     #requests library has objects that helps with this, eg HTTPBasicAuth
     basic = HTTPBasicAuth(kaggle_username, kaggle_apikey)
     #we are going to get a request on this endpoint (URL). 
     #Stream= True is required for unziping the file as it has to be in stream format for the zipfile library to handle
-    response = requests.get('https://www.kaggle.com/api/v1/datasets/download/evangower/valorant-esports-top-earnings', auth = basic, stream=True)
+    response = requests.get('https://www.kaggle.com/api/v1/datasets/download/' + kaggle_dataset_path, auth = basic, stream=True)
 
     #error checking, exit() exists the program entirely
     if response.status_code == 401:
--- valorant_stats.py
+++ valorant_stats.py
@@ -1,5 +1,6 @@
 import os
 from pull_dataset import pull
+import pandas as pd 
 
 #reads the .env file and sets each line of the code as environment variable
 #I need this bc I would be putting secrets (my username and key) which should stay hidden
@@ -7,5 +8,17 @@ from pull_dataset import pull
 from dotenv import load_dotenv
 load_dotenv()
 
-pull(os.environ['KAGGLE_USERNAME'], os.environ['KAGGLE_APIKEY'])
+pull(os.environ['KAGGLE_USERNAME'], os.environ['KAGGLE_APIKEY'], os.environ['KAGGLE_DATASET_PATH'])
 
+# player_stats = pd.read_csv('data/Players.csv')
+# team_stats = pd.read_csv('data/Teams.csv')
+# print(team_stats)
+
+player_stats = pd.read_csv('data/val_stats.csv')
+
+#print(player_stats)
+
+agent_stats = player_stats[['agent_1', 'win_percent']].copy()
+agent_median = agent_stats.groupby('agent_1').median()
+
+print(agent_median)
